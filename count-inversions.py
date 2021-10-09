8cadb508f9d0fc0071b26458806df28d01f7d625
amanlp7 <ak547276@gmail.com>
2021-10-09 13:32:15 +0530

    Add new script

---
+++ count-inversions.py
@@ -0,0 +1,31 @@
+####################################### Python script to count nuumber of inversions ###################################
+
+'''
+Implementation of algorithm which count the numbers of inversions in
+and array.
+'''
+
+########################################################################################################################
+
+class CountInversions:
+    '''
+    Class containing methods to count number of inversions
+    ...
+
+    Attributes
+    ----------
+    None
+    '''
+
+    def __init__(self):
+        '''
+        Function to instantiate the object of the class
+        ...
+
+        Parameters
+        ----------
+        None
+        '''
+
+
+    
