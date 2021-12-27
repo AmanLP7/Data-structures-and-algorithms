26395a46368bf91d2763c5bd7381ba888fc10fd3
amanlp7 <ak547276@gmail.com>
2021-12-27 14:09:16 +0530

    Slow solution

--- kargers-min-cut.py
+++ kargers-min-cut.py
@@ -10,6 +10,8 @@ This is a python script to find minmum number of cuts in graph using Karger's ra
 
 # System library imports 
 import random                   # Contain functions to make random selections
+import math                     # Mathematical functions
+import copy                     # Module contains function to create copies of an object
 
 
 ########################################################################################################################
@@ -53,33 +55,34 @@ class KargersMinCut:
 
         with open(file, "r") as adj:
             for line in adj:
-                l = list(map(int, line.strip().split("  ")))
+                l = list(map(int, line.strip().split(" ")))
                 self.graph[l[0]] = l[1:]
 
         return self.graph
 
 
-    def choose_random_edge(self) -> tuple:
+    def choose_random_edge(self, graph: dict) -> tuple:
         '''
         Function to choose a random edge
         ...
 
         Parameters
         ----------
-        None
+        graph (dict):
+            A graph as a dictionary
 
         Returns
         -------
         A tuple containing nodes connected by the edge
         '''
         
-        node1 = random.choice(list(self.graph.keys()))
-        node2 = random.choice(self.graph[node1])
+        node1 = random.choice(list(graph.keys()))
+        node2 = random.choice(graph[node1])
 
         return (node1, node2)
 
 
-    def random_contraction(self) -> int:
+    def random_contraction(self, graph: dict) -> int:
         '''
         Function find the minimum number of cuts in a graph
         using random contraction.
@@ -87,7 +90,8 @@ class KargersMinCut:
 
         Parameters
         ----------
-        None
+        graph (dict):
+            A graph as dictionary
 
         Returns
         -------
@@ -96,27 +100,27 @@ class KargersMinCut:
 
         # Keep contracting the edges until
         # no more than 2 nodes remain
-        while len(self.graph) > 2:
+        while len(graph) > 2:
             
             # Choose random edge
-            v1, v2 = self.choose_random_edge()
+            v1, v2 = self.choose_random_edge(graph)
 
             # Add elements of v2 to v1, for each node 
             # connected to v2, delete connection to v2
             # and add connection to v1
-            self.graph[v1].extend(self.graph[v2])
-            for node in self.graph[v2]:
-                self.graph[node].remove(v2)
-                self.graph[node].append(v1)
+            graph[v1].extend(graph[v2])
+            for node in graph[v2]:
+                graph[node].remove(v2)
+                graph[node].append(v1)
 
             # Delete node v2
-            del self.graph[v2]
+            del graph[v2]
 
             # Remove self loops for node v1
-            while v1 in self.graph[v1]:
-                self.graph[v1].remove(v1)
+            while v1 in graph[v1]:
+                graph[v1].remove(v1)
 
-        minimum_cuts = len(self.graph[list(self.graph.keys())[0]])
+        minimum_cuts = len(graph[list(graph.keys())[0]])
 
         return minimum_cuts
 
@@ -129,12 +133,12 @@ if __name__ == '__main__':
 
     kargers = KargersMinCut()
 
-    G = kargers.create_graph("test.txt")
-
+    # List to hold minimum cuts for each iteration
     cuts = []
 
-    for _ in range(1000):
-        min_cuts = kargers.random_contraction()
+    for _ in range(300):
+        graph = kargers.create_graph("test.txt")
+        min_cuts = kargers.random_contraction(graph)
         cuts.append(min_cuts)
     
-    print(set(cuts))
+    print(f"Minimum number of cuts: {min(cuts)}\n")
