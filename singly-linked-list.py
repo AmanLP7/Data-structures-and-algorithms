2ad39317ba984f1247c06353ebcce243b194bd97
amanlp7 <ak547276@gmail.com>
2022-01-10 21:17:59 +0530

    Added push and insert after node functions.

--- singly-linked-list.py
+++ singly-linked-list.py
@@ -78,6 +78,81 @@ class LinkedList:
             print(element.data)
             element = element.next
 
+
+    def push(self, value: Union[int, float]) -> None:
+        '''
+        Function to add new node at the front of a singly
+        linked list. The time complexity of this operation
+        is O(1).
+        ...
+
+        Parameters
+        ----------
+        value (int or float):
+            value to be inserted
+
+        Returns
+        -------
+        None
+        '''
+
+        # Create a new node element
+        new_node = Node(value)
+
+        # Point the new node to the head of linked list
+        new_node.next = self.head
+
+        # Make the value as the new head of linked list
+        self.head = new_node 
+
+
+    def insert_after_node(self, prev_node: object, value) -> Union[None, str]:
+        '''
+        Function to add a new node after a specified node.
+        The time complexity of this function is O(1).
+        ...
+
+        Parameters
+        ----------
+        prev_node (object):
+            node object after which the new value will be added
+        value (int or float):
+            new value to be added
+
+        Returns
+        -------
+        If prev node is not found then return string else None
+        '''
+
+        # Checking of the previous node exists
+        if prev_node is None:
+            return "Node must exist in the linke list..."
+
+        # Create a new node object
+        new_node = Node(value)
+
+        # Point the new node to the next value 
+        # of the previous node
+        new_node.next = prev_node.next
+
+        # Point the previous node to the new node
+        prev_node.next = new_node
+
+
+    def append(self, value: Union[int, float]) -> Union[None, str]:
+        '''
+        Function to add a new node to the end of a linked list.
+        The time complexity of this operation is O(n), where n
+        is the number of nodes in the linked list.
+        ...
+
+        Parameters
+        ----------
+        value (int, float):
+
+        '''
+
+
 ########################################################################################################################
 
 if __name__ == '__main__':
@@ -95,6 +170,16 @@ if __name__ == '__main__':
     llist.head.next = second
     second.next = third
 
+    # Adding new element at the front
+    # of the linked list
+    llist.push(4)
+    print("Modified list...\n")
+    llist.print_linked_list()
+
+    # Adding new element after an
+    # existing node
+    llist.insert_after_node(second, 7)
+    print("Node added in between the linked list...\n")
     llist.print_linked_list()
 
 
